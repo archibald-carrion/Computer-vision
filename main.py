4dbe58a7e8276076c039a8418a90160fa749fb9f
archibald-carrion <archibald.carrion@gmail.com>
2022-02-28 17:54:12 -0600

    image reading code
    some code for image reading have been added

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-import cv2
+import cv2 #OpenCV package
+
+img = cv2.imread('images\img1.png', cv2.COLOR_BGR2RGB)
+img = cv2.resize(img,(0,0), fx=0.5,fy=0.5)
+
+cv2.imshow('image', img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+cv2.imwrite('new_img.png',img) #create a new iamge with the modification
