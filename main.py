4cf7d422bffce76d1fbc73cd986fc900a6a01733
archibald-carrion <archibald.carrion@gmail.com>
2022-03-03 14:46:53 -0600

    .xml code added
    i've added a new .xml code from Intel : https://github.com/opencv/opencv/tree/master/data/haarcascades

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 import cv2 #OpenCV package
 
-img = cv2.imread('images\img1.png', cv2.COLOR_BGR2RGB)
-img = cv2.resize(img,(0,0), fx=0.5,fy=0.5)
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+#img = cv2.imread('images/jeff bezos.jpg', cv2.COLOR_BGR2GRAY)
+#img = cv2.imread('images/photo.jpg', cv2.COLOR_BGR2GRAY)
+img = cv2.imread('images/v.jpg', cv2.COLOR_BGR2GRAY)
+
+faces = face_cascade.detectMultiScale(img, 1.3,4)
+#print("Hello world")
+
+for (x , y , w , h) in faces:
+    cv2.rectangle(img, (x,y), (x+w, y+h), (255, 0, 0), 2)
 
 cv2.imshow('image', img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
-
-cv2.imwrite('new_img.png',img) #create a new iamge with the modification
