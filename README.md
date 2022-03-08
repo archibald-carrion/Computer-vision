a2d8d92cacf0711566480b19761799824904821a
archibald-carrion <archibald.carrion@gmail.com>
2022-03-08 09:44:45 -0600

    i've changed the xml file

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # computer-vision
 Python code using computer vision to detect if there is a human on a photo
 
-i will be using OpenCV, to install OpenCV on Windows10 use this command line in CMD :
-pip install opencv-python
+I use OpenCV, to install OpenCV on Windows10 I use this command line in CMD :
+    pip install opencv-python
+
+The haar cascade I use come from the OpenCV Github directory
+    https://github.com/opencv/opencv/tree/master/data/haarcascades
--- main.py
+++ main.py
@@ -2,9 +2,9 @@ import cv2 #OpenCV package
 
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
-#img = cv2.imread('images/jeff bezos.jpg', cv2.COLOR_BGR2GRAY)
+img = cv2.imread('images/jeff bezos.jpg', cv2.COLOR_BGR2GRAY)
 #img = cv2.imread('images/photo.jpg', cv2.COLOR_BGR2GRAY)
-img = cv2.imread('images/v.jpg', cv2.COLOR_BGR2GRAY)
+#img = cv2.imread('images/v.jpg', cv2.COLOR_BGR2GRAY)
 
 faces = face_cascade.detectMultiScale(img, 1.3,4)
 #print("Hello world")
@@ -15,3 +15,5 @@ for (x , y , w , h) in faces:
 cv2.imshow('image', img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+
+
