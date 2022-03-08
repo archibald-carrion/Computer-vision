727ddb36ccd437abb7337957f2538eedb464162f
archibald-carrion <archibald.carrion@gmail.com>
2022-03-08 16:36:40 -0600

    modification to the main code
    i've added a loop so the code can loop and show to the user every image in the folder

--- main.py
+++ main.py
@@ -1,28 +1,28 @@
-import cv2 #OpenCV package
-#import time
-#from datetime import datetime
+import cv2
 
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+images = ["images/linus_torvald.jpg", "images/photo.jpg", "images/star_wars.png", "images/v_cyberpunk.webp", "images/elon_musk.png","images/jeff_bezos.jpg",]
 
 #img = cv2.imread('images/jeff bezos.jpg', cv2.COLOR_BGR2GRAY)
-img = cv2.imread('images/star_wars.png', cv2.COLOR_BGR2GRAY)
-#img = cv2.imread('images/photo.jpg', cv2.COLOR_BGR2GRAY)
-#img = cv2.imread('images/v.jpg', cv2.COLOR_BGR2GRAY)
 
-faces = face_cascade.detectMultiScale(img, 1.3,4)
-#print("Hello world")
+counter = 0
+continueCode = True
 
-for (x , y , w , h) in faces:
-    cv2.rectangle(img, (x,y), (x+w, y+h), (255, 0, 0), 2)
-
-#tick = 60
-contador = 0
-while contador<3000000 :
+while counter < 6 and continueCode:
+    img = cv2.imread(images[counter], cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(img, 1.3,4)
+    for (x , y , w , h) in faces:
+        cv2.rectangle(img, (x,y), (x+w, y+h), (255, 0, 0), 2)
+    
     cv2.imshow('image', img)
-    contador = contador +1
-#cv2.waitKey(0)
-#how to show the image for 5 second without having a "grey screen" ?
-#time.sleep(5)
-cv2.destroyAllWindows()
+    key = cv2.waitKey(2000)
 
-
+    if key == 27:
+        cv2.destroyAllWindows()
+        continueCode = False
+    else:            
+        cv2.destroyWindow("image")
+        if counter == 5:
+            counter = 0
+        else:
+            counter = counter + 1
