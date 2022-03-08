f6cb9d2728e0fce27f81ab9253eda8fdb11950ab
archibald-carrion <archibald.carrion@gmail.com>
2022-03-08 10:28:59 -0600

    i've added a few image for test

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import cv2 #OpenCV package
+#import time
+#from datetime import datetime
 
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
-img = cv2.imread('images/jeff bezos.jpg', cv2.COLOR_BGR2GRAY)
+#img = cv2.imread('images/jeff bezos.jpg', cv2.COLOR_BGR2GRAY)
+img = cv2.imread('images/star_wars.png', cv2.COLOR_BGR2GRAY)
 #img = cv2.imread('images/photo.jpg', cv2.COLOR_BGR2GRAY)
 #img = cv2.imread('images/v.jpg', cv2.COLOR_BGR2GRAY)
 
@@ -12,8 +15,14 @@ faces = face_cascade.detectMultiScale(img, 1.3,4)
 for (x , y , w , h) in faces:
     cv2.rectangle(img, (x,y), (x+w, y+h), (255, 0, 0), 2)
 
-cv2.imshow('image', img)
-cv2.waitKey(0)
+#tick = 60
+contador = 0
+while contador<3000000 :
+    cv2.imshow('image', img)
+    contador = contador +1
+#cv2.waitKey(0)
+#how to show the image for 5 second without having a "grey screen" ?
+#time.sleep(5)
 cv2.destroyAllWindows()
 
 
