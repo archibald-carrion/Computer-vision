8a4a6f0def17bf382505d44574d4c4746d44fbdc
archibald-carrion <archibald.carrion@gmail.com>
2022-03-10 08:16:39 -0600

    code modification
    the main script is now a function called "detectHuman" and takes 1 parameter : the array that contain the path to the photos/images
    
    i've also added a few image for test

--- main.py
+++ main.py
@@ -1,28 +1,31 @@
 import cv2
-
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-images = ["images/anti-facerecognition2.webp", "images/anti-facerecognition1.webp", "images/linus_torvald.jpg", "images/photo.jpg", "images/star_wars.png", "images/v_cyberpunk.webp", "images/elon_musk.png","images/jeff_bezos.jpg",]
-
+images = ["images/linus_torvald.jpg","images/star_wars.png", "images/v_cyberpunk.png", "images/elon_musk.png","images/jeff_bezos.jpg","images/anti_detection1.png","images/anti_detection2.png","images/anti_detection3.png","images/anti_detection4.png","images/terminator.png"]
 #img = cv2.imread('images/jeff bezos.jpg', cv2.COLOR_BGR2GRAY)
 
-counter = 0
-continueCode = True
+def detectHuman(theArray):
+    counter = 0
+    continueCode = True
+    arrayLength = len(theArray)
 
-while counter < 6 and continueCode:
-    img = cv2.imread(images[counter], cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(img, 1.1,4)
-    for (x , y , w , h) in faces:
-        cv2.rectangle(img, (x,y), (x+w, y+h), (255, 0, 0), 2)
-    
-    cv2.imshow('image', img)
-    key = cv2.waitKey(2000)
+    while counter < arrayLength and continueCode:
+        img = cv2.imread(images[counter], cv2.COLOR_BGR2GRAY)
+        faces = face_cascade.detectMultiScale(img, 1.2,4)
+        for (x , y , w , h) in faces:
+            cv2.rectangle(img, (x,y), (x+w, y+h), (255, 0, 0), 2)
+        
+        cv2.imshow('image', img)
+        key = cv2.waitKey(2000)
 
-    if key == 27:
-        cv2.destroyAllWindows()
-        continueCode = False
-    else:            
-        cv2.destroyWindow("image")
-        if counter == 5:
-            counter = 0
-        else:
-            counter = counter + 1
+        if key == 27:
+            cv2.destroyAllWindows()
+            continueCode = False
+        else:            
+            cv2.destroyWindow("image")
+            if counter == arrayLength-1:
+                counter = 0
+            else:
+                counter = counter + 1
+
+detectHuman(images)
+
