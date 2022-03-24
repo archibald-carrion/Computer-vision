b8e140664070a266c041e2c338b9d176b6c86ad7
archibald-carrion <archibald.carrion@gmail.com>
2022-03-23 18:18:21 -0600

    Update main_minimalist.py

--- main_minimalist.py
+++ main_minimalist.py
@@ -1,10 +1,10 @@
-import cv2
+import cv2 
 from cv2 import destroyAllWindows
 face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 image = cv2.imread("images/v_cyberpunk.png", cv2.COLOR_BGR2GRAY)
-faces = face_cascade.detectMultiScale(image, 1.2,4)
+faces = face_cascade.detectMultiScale(image,1.2,4)
 for (x,y,w,h) in faces:
     cv2.rectangle(image,(x,y),(x+w,y+h),(0,0,255),2)
-cv2.imshow("Image",image)
+cv2.imshow("Mon image", image)
 cv2.waitKey(0)
 destroyAllWindows()
